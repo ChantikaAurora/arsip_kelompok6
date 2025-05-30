7b4667640dcff71864bb9bcf46d48e30dd6917c7
ChantikaAurora <chantikaakmal@gmail.com>
2025-05-30 23:24:51 +0700

    Perubahan pada routes/web.php sebelum pull

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\PenggunaController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\JenisArsipController;
 use App\Http\Controllers\SuratKeluarController;
+use App\Http\Controllers\SuratMasukController;
+
 
 Route::get('/home', function () {
     return view('welcome');
@@ -23,10 +25,10 @@ Route::post('/',[LoginController::class,'authenticate']);
 Route::resource('/suratkeluar', SuratKeluarController::class);
 Route::get('/suratkeluar/download/{id}', [SuratKeluarController::class, 'download'])->name('suratkeluar.download');
 
-Route::post('/logout', function () {
-    Auth::logout();
-    request()->session()->invalidate();
-    request()->session()->regenerateToken();
-    return redirect('/');
-})->name('logout');
+// Route::post('/logout', function () {
+//     Auth::logout();
+//     request()->session()->invalidate();
+//     request()->session()->regenerateToken();
+//     return redirect('/');
+// })->name('logout');
 
