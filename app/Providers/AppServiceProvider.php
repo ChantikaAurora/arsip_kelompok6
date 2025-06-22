7e6a66b4be99a47f43384b84d57848a111257b19
ChantikaAurora <chantikaakmal@gmail.com>
2025-06-22 16:16:57 +0700

    menyelesaikan conflict pada iterasi-3

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
          Paginator::useBootstrap();
-         Livewire::component('dropdown-jurusan-prodi', DropdownJurusanProdi::class);
+        //  Livewire::component('dropdown-jurusan-prodi', DropdownJurusanProdi::class);
     }
 }
